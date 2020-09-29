adb78fea5849ea7afb4bf592f83cff2a16bc98de
Priya Power <priyajag72@gmail.com>
2020-09-29 14:36:09 -0600

    Add JSON require to near_earth_objects

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -1,6 +1,8 @@
 require 'faraday'
 require 'figaro'
 require 'pry'
+require 'json'
+
 # Load ENV vars via Figaro
 Figaro.application = Figaro::Application.new(environment: 'production', path: File.expand_path('../config/application.yml', __FILE__))
 Figaro.load
