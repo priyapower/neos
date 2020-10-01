f69813b26eaa4a339cbb268e1f648fbccf0f9855
Priya Power <priyajag72@gmail.com>
2020-10-01 11:47:23 -0600

    Update test for test it exists

--- near_earth_objects_test.rb
+++ near_earth_objects_test.rb
@@ -9,20 +9,25 @@ class NearEarthObjectsTest < Minitest::Test
     @neos_from_dynamic_date_vs_2 = NearEarthObjects.new('2020-01-01')
   end
 
+  def test_it_exists
+    assert_instance_of NearEarthObjects, @neos_from_dynamic_date
+    assert_instance_of NearEarthObjects, @neos_from_dynamic_date_vs_2
+  end
+
   def test_a_date_returns_a_list_of_neos
     assert_equal '(2020 SN2)', @neos_from_dynamic_date.neo_data[0][:name]
   end
 
-  def test_largest_asteroid_returns_one_integer_max_value
+  def test_it_can_return_largest_asteroid_with_max_integer_value
     assert_equal 1949, @neos_from_dynamic_date.largest_asteroid
   end
 
-  def test_it_can_return_neos_from_date_given
+  def test_it_can_return_count_neos_from_date_given
     assert_equal 12, @neos_from_dynamic_date.neo_count
     assert_equal 13, @neos_from_dynamic_date_vs_2.neo_count
   end
 
-  def test_it_can_format_data
+  def test_it_can_format_data_for_print
     assert_equal "253 ft", @neos_from_dynamic_date.format_data[0][:diameter]
   end
 end
