897af1b6c0569b86d6a258b2662a33248ccd2cc4
Priya Power <priyajag72@gmail.com>
2020-10-01 11:47:44 -0600

    Update start after debugging through errors and naming from updated near_earth_objects file

--- start.rb
+++ start.rb
@@ -1,29 +1,21 @@
 require_relative 'near_earth_objects'
-require 'pry'
 
 puts "________________________________________________________________________________________________________________________________"
-puts "Welcome to NEO. Here you will find information about how many meteors, astroids, comets pass by the earth every day. \nEnter a date below to get a list of the objects that have passed by the earth on that day."
+puts "Welcome to NEO. Here you will find information about how many meteors, asteroids, comets pass by the earth every day. \nEnter a date below to get a list of the objects that have passed by the earth on that day."
 puts "Please enter a date in the following format YYYY-MM-DD."
 print ">>"
 
 date = gets.chomp
-
-# asteroid_details = NearEarthObjects.find_neos_by_date(date)
 asteroid_details = NearEarthObjects.new(date)
-
-asteroid_list = asteroid_details.neo_data
-total_number_of_asteroids = asteroid_details.neo_count
-largest_asteroid = asteroid_details.largest_asteroid
+formatted_asteroid_info = asteroid_details.format_data
+formated_date = DateTime.parse(date).strftime("%A %b %d, %Y")
 
 column_labels = { name: "Name", diameter: "Diameter", miss_distance: "Missed The Earth By:" }
 column_data = column_labels.each_with_object({}) do |(col, label), hash|
   hash[col] = {
-    label: label,
-    sdkhjdslfkjsdflksjdflksdjfdslkj
-    # Left off: make below it's own method EXCLUDING THE 'label.size'
-    width: [asteroid_list.map do |asteroid|
-      asteroid[col].size
-    end.max, label.size].max}
+              label: label,
+              width: [formatted_asteroid_info.map { |asteroid| asteroid[col].size }.max, label.size].max
+              }
 end
 
 header = "| #{ column_data.map { |_,col| col[:label].ljust(col[:width]) }.join(' | ') } |"
@@ -35,15 +27,14 @@ def format_row_data(row_data, column_info)
 end
 
 def create_rows(asteroid_data, column_info)
-  rows = asteroid_data.each { |asteroid| format_row_data(asteroid, column_info) }
+  asteroid_data.each { |asteroid| format_row_data(asteroid, column_info) }
 end
 
-formated_date = DateTime.parse(date).strftime("%A %b %d, %Y")
 puts "______________________________________________________________________________"
-puts "On #{formated_date}, there were #{total_number_of_asteroids} objects that almost collided with the earth."
-puts "The largest of these was #{largest_asteroid} ft. in diameter."
+puts "On #{formated_date}, there were #{asteroid_details.neo_count} objects that almost collided with the earth."
+puts "The largest of these was #{asteroid_details.largest_asteroid} ft. in diameter."
 puts "\nHere is a list of objects with details:"
 puts divider
 puts header
-create_rows(asteroid_list, column_data)
+create_rows(formatted_asteroid_info, column_data)
 puts divider
